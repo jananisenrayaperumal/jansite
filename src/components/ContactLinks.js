62095b4de3f4aa10cfce883bc7de195b07d13698
Janani <jananisenrayaperumal@gmail.com>
2025-02-23 18:00:54 -0500

    updated linked

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -5,7 +5,7 @@ const ContactLinks = () => {
 		<div className="flex items-center justify-center gap-4 mt-10">
 			{/* GitHub */}
 			<a
-				href="https://github.com/JiyaJanani"
+				href="https://github.com/jananisenrayaperumal"
 				target="_blank"
 				rel="noopener noreferrer"
 				className="flex items-center px-4 py-2 bg-gray-800 text-white rounded-lg shadow hover:bg-gray-700 transition">
@@ -14,7 +14,7 @@ const ContactLinks = () => {
 
 			{/* LinkedIn */}
 			<a
-				href="https://www.linkedin.com/in/jiya-janani/"
+				href="https://www.linkedin.com/in/jananisenrayaperumal/"
 				target="_blank"
 				rel="noopener noreferrer"
 				className="flex items-center px-4 py-2 bg-linkedin-blue text-white rounded-lg shadow hover:bg-blue-500 transition">
@@ -23,7 +23,7 @@ const ContactLinks = () => {
 
 			{/* Resume */}
 			<a
-				href="https://drive.google.com/file/d/1yeqEMqk1x8ccr0H22QfoQaJ913jbKe6l/view?usp=sharing"
+				href="https://docs.google.com/document/d/e/2PACX-1vTRpb06in5BxOebhJABv2L-EOgKoSUXRRzJzG0_EiAwMWLP1xBM5MMk_NnXTbVLsJHS2S-Nx0pwRBXs/pub"
 				target="_blank"
 				download
 				className="flex items-center px-4 py-2 bg-green-600 text-white rounded-lg shadow hover:bg-green-500 transition">
