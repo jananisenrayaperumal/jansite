7c120ef2932c8ef5486f28543fb621a861f2545f
Janani <jananisenrayaperumal@gmail.com>
2025-02-23 18:00:42 -0500

    animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
 	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			animation: {
+				fadeUp: "fadeUp 0.8s ease-out forwards" // Duration of the animation
+			},
+			keyframes: {
+				fadeUp: {
+					"0%": { opacity: "0", transform: "translateY(20px)" }, // Start from below and invisible
+					"100%": { opacity: "1", transform: "translateY(0)" } // End at normal position with full opacity
+				}
+			},
 			colors: {
 				linkedin: {
 					blue: "#0077B5",
